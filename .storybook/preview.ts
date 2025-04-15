8d73213dd274e73bba7b516405c6d881e6940a0f
ricardogdf <ricardogf2004@gmail.com>
2025-04-14 23:09:16 -0300

    Adicionando modo dark e light no storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,6 +8,13 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
+    themes: {
+      default: 'light',
+      list: [
+        { name: 'light', class: 'light', color: '#ffffff' },
+        { name: 'dark', class: 'dark', color: '#000000' }
+      ]
+    }
   },
 };
 
